08c2672522350edccf08b27ea3562c6ea8514f5c
Ana Evelyn <evelynholanda@gmail.com>
2025-04-04 13:02:19 -0300

    Update all Pipeline

--- support/db.js
+++ support/db.js
@@ -6,12 +6,12 @@ const db = pgp('postgresql://dba:dba@paybank-db:5432/UserDB');
 
 export async function obterCodigo2FA(cpf) {
     const query = `
-        SELECT t.code
-	    FROM public."TwoFactorCode" t
-		JOIN public."User" u ON u."id" = t."userId"
-		WHERE u."cpf" = '${cpf}'
-	    ORDER BY t.id DESC
-	    LIMIT 1;
+
+        SELECT code
+        FROM public."TwoFactorCode"
+        ORDER BY id DESC
+        LIMIT 1;
+        
     `
     const result = await db.oneOrNone(query) 
     return result.code   
